15353f04c5e3941dfdf4e56e9abf67fceb62a502
sdariza <sdariza@uninorte.edu.co>
2025-02-22 23:15:57 -0500

    Remove redundant test case for node 10 in ABBTest

--- src/test/java/ABBTest.java
+++ src/test/java/ABBTest.java
@@ -16,6 +16,5 @@ public class ABBTest {
         assertEquals("The node 40 should not be in the tree", false, tree.searchABB(tree.getRoot(), 40));
         assertEquals("The node 5 should be in the tree", true, tree.searchABB(tree.getRoot(), 5));
         assertEquals("The node 15 should be in the tree", true, tree.searchABB(tree.getRoot(), 15));
-        assertEquals("The node 10 should be in the tree", true, tree.searchABB(tree.getRoot(), 10));
     }
 }
